5046e981e0b8877b76298ceb2ddafc6949bf528b
Khimya <khimyakhetarpal@gmail.com>
2014-04-09 14:34:32 +0530

    Create ex1.py

---
+++ ex1.py
@@ -0,0 +1,6 @@
+name   =  str(raw_input("Please enter your name: "))
+age    =  int(raw_input("Please enter your age: "))
+years = 100 - age
+year  = 2014 + years
+print(" Hello " + name + " , You will turn 100 years old in the year") 
+print year
