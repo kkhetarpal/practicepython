f036f7ffc607cc05dbf4c7d4bc5d48d538121717
Khimya <khimyakhetarpal@gmail.com>
2014-04-09 17:06:13 +0530

    adding a buggy program

---
+++ greetings.py
@@ -0,0 +1,16 @@
+x = raw_input("What is the time?")
+
+if x<10:
+    print "Good Morning: Have a pretty day ahead"
+
+elif x<12:
+    print "Time for Lunch Soon: Bon Appetite"
+
+elif x<18:
+    print "Hope you are having a Good Day"
+
+elif x<22:
+    print "Good Evening"
+
+else:
+    print "Good Night: Sweet Dreams" 
